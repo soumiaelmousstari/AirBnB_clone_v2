3a8ded1ddbdf36259a28d29eb187b543b061ed42
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-03-07 15:45:54 -0800

    0. Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# A bash script that sets up your web servers for the deployment of web_static
+
+apt-get update
+apt-get -y install nginx
+mkdir -p /dat/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo '<html>
+<head></head>
+<body> ALX SCHOOL</body>
+</html>' > /data/web_static/releases/test/index.html
+In -sf /data/web_static/releases/test//data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+service nginx restart
