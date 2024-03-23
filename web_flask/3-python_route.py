566887f0b6f9adb55fda5a5eee35c4f4ee2f1cdc
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-03-23 08:50:56 -0700

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+3.Python is cool!(a script that starts a Flask web applicatio)
+"""
+from flask import Flask
+application = Flask(__name__)
+
+
+@application.route('/', strict_slashes=False)
+def Hello_HBNB():
+    "Her return is 'Hello HBNB!'"
+    return 'Hello HBNB!'
+
+
+@application.route('/hbnb', strict_slashes=False)
+def fun_HBNB():
+    "Here return is 'HBNB'"
+    return 'HBNB'
+
+
+@application.route('/C/<text>', strict_slashes=False)
+def C_fun(text):
+    """
+    Her return is 'C followed by the valye of the text variable
+    replace(_ withe space)'
+    """
+    return 'C ' + text.raplace('_', ' ')
+
+
+@application.route('/python/')
+@application.route('/python/<text>', strict_slashes=False)
+def python_fun(text):
+    """
+    Her return is 'Python followd by text's value(replace _ with space)'
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+if '__name__' == '__main__':
+    application.run(host='0.0.0.0', port='5000')
