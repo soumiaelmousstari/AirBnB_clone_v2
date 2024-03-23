8aa2eff768e99ff1c1699df8ca6d9c6b3e7b5314
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-03-23 08:22:44 -0700

    2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+2.C is fun!(a script that starts a Flask web application)
+"""
+from flask import Flask
+application = Flask(__name__)
+
+
+@application.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """Her return is 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@application.route('/hbnb', strict_slashes=False)
+def fun_HBNB():
+    """Her return is 'HBNB'"""
+    return 'HBNB'
+
+
+@application.rout('/c/<text>', strict_slashes=False)
+def c_fun():
+    """
+    Her return is 'C followed by the value of the
+    text variable (replace underscore _ with a space)'
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port='5000')
